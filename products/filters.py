5aa47eada07e20f5944e2feb1ccb86a59b6a08ba
Ark005 <005ark@gmail.com>
2020-12-22 17:51:05 +0300

    много выдачи

--- products/filters.py
+++ products/filters.py
@@ -5,9 +5,9 @@ from django_filters.filters import RangeFilter
 
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
-    price = RangeFilter()
+    #price = RangeFilter()
 
 
     class Meta:
         model = Product
-        fields = ['name', 'price']
+        fields = ['name']
